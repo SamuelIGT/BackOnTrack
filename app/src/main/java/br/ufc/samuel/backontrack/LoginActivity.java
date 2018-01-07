0325472fef383e4d6037e5efa6dbbcd7ad544cb9
SamuelIGT <samuel.br.igt@gmail.com>
2018-01-07 18:55:40 -0200

    Back On Track v07.01.18
    *XML Login Screen done.
    *Login Activity code preconfigured.

---
+++ app/src/main/java/br/ufc/samuel/backontrack/LoginActivity.java
@@ -0,0 +1,47 @@
+package br.ufc.samuel.backontrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtRegistration;
+    private EditText edtPassword;
+    //private ImageButton btnLogin;
+    private TextView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViews();
+    }
+
+    private void findViews() {
+        edtRegistration = findViewById(R.id.editText_registration);
+        edtPassword = findViewById(R.id.editText_password);
+        //btnLogin = findViewById(R.id.btn_login);
+        logo = findViewById(R.id.text_login_logo);
+    }
+
+    public void login(View view) {
+        String registration;
+        String password;
+
+        if(edtRegistration.getText() != null){
+            registration = edtRegistration.getText().toString();
+        }
+        if (edtPassword.getText() != null){
+            password = edtPassword.getText().toString();
+        }
+
+        //TODO: Realizar chamada de metodo de login do servidor.
+        //TODO: Se o login for válido guardar no banco local o login e senha.
+    }
+}
